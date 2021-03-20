4a2c08773c88d64320c90d8dcd3d6e8843e44e71
Sébastien <seb.lasante@hotmail.com>
2021-03-20 21:32:55 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 
 function Navbar() {
     return (
-        <div className='navbar'>
+        <div className='navi'>
             <nav>
                 <ul className='ul-nav'>
                     <li className='li-nav'>
