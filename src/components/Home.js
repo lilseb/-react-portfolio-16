ea8cc677c4053526b662957a1ece88e86f8f483d
Sébastien <seb.lasante@hotmail.com>
2021-03-22 08:56:57 +0100

    video

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import future from '../img/future.mp4';
 
 function Home() {
     return (
-        <div>
-            hello world
+        <div className='Home'>
+            <div>
+              <h1 className='hello'>Hello, World</h1>
+              <h1 className='hello'>My name is Seb!</h1>
+            </div>
+            {/* <img className=''/> */}
+            <video className='vid-future' autoPlay loop muted>
+                <source className='future' src={future} type='video/mp4' />
+            </video>
         </div>
     );
 }
